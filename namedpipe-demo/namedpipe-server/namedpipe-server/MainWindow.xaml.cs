632d7d100d3b3b8d8c87f3771ab5c65102cd7935
Vinson <ykx.vinson@gmail.com>
2022-07-28 14:58:54 +0800

    Add namedPipe server/client base on C#

---
+++ namedpipe-demo/namedpipe-server/namedpipe-server/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace namedpipe_server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NamedPipeServer m_serverNamedPipe;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(() =>
+            {
+                m_serverNamedPipe = new NamedPipeServer("Presnter_NamedPipe");
+                m_serverNamedPipe.Readed += OnPipeReadMsg;
+                m_serverNamedPipe.Start();
+            });
+        }
+
+        /// <summary>
+        /// 收到管道数据
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public string OnPipeReadMsg(string arg)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                this.lstbox.Items.Add(arg);
+            });
+            return "";
+        }
+    }
+}
